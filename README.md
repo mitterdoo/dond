db84ded95eccf2d87f590d6c26f5c0226eebc412
mitterdoo <connorashcroft@live.com>
2016-08-30 06:16:25 -0700

    Set up Github Pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# dond
-Deal or No Deal Content
-just some sounds for my deal or no deal game on metastruct
--- lua/autorun/server/dond_content.lua
+++
@@ -1,4 +0,0 @@
-local files = file.Find( "sound/dond/*.*", "GAME" )
-for k, v in pairs( files ) do
-	resource.AddFile( "sound/dond/" .. v )
-end
